1a427caf82fd0dac8b5bb78f744c67f2d1204b87
Narayanasriman <v.d.s.narayana2002@gmail.com>
2025-04-15 10:52:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
-import React,{useEffect, useState }from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [input,setInput]=useState("");
-  const [result,setResult]=useState([])
+  const [input,setInput]=useState("")
+  const [result,setResult]=useState([]);
   const [showResults,setShowResults]=useState(false);
-  const [cache,setCache]=useState({});
+  const [cache,setCache]=useState({})
   const fetchData=async()=>{
     if(cache[input]){
-      console.log('Cache Returned',input)
+      console.log('Cache returned',input);
       setResult(cache[input]);
       return;
+      
     }
-    console.log('Api-call',input)
-    const data=await fetch("https://dummyjson.com/recipes/search?q=" + input);
+    const data=await fetch('https://dummyjson.com/recipes/search?q=' + input);
     const json=await data.json();
     setResult(json?.recipes);
-    setCache((prev)=>({...prev,[input]: json?.recipes}));
-
-  };
+    setCache((prev)=>({...prev,[input]:json?.recipes}))
+    console.log('Api call:'+ input)
+  }
   useEffect(()=>{
-    const timer=setTimeout(fetchData,300);
-    return ()=>{
+    const timer=setTimeout(fetchData,300)
+    return (()=>{
       clearTimeout(timer)
-    }
-   
-  },[input]);
+    })
+    
+  },[input])
+  
   return (
     <div className='App'>
-      <h1>Search Engine Optimisation</h1>
-      <div>
+      <h1>Search Optimisation</h1>
       <input 
+      value={input} 
       type='text'
-      className='search-input'
-      value={input}
-      onChange={(e)=>setInput(e.target.value)}
-      onFocus={(e)=>setShowResults(true)}
-      onBlur={()=>setShowResults(false)}
-      
-      ></input>
-
-      {showResults && (
-        <div className='results-container'>
-        {result.map((r)=>(<span  className='results' key={r.id}>{r.name}</span>))}
+       className='input' 
+       onChange={(e)=>setInput(e.target.value)}
+       onFocus={(e)=>setShowResults(true)}
+       onBlur={()=>setShowResults(false)}/>
+       {showResults && (
+        <div className='result-container'>
+        {result.map((value)=>(<span key={value.id} className='result'>{value.name}</span>))}
+        
       </div>
 
-      )}
+       )}
       
-     
-
-      </div>
       
     </div>
   )
